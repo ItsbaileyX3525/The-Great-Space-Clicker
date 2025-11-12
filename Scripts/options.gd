6bec91fdfba0c90e091800ed5d336c5f7abab796
Bailey Miles <baileyleah542@gmail.com>
2025-11-12 01:26:46 +0000

    Add background music to settings and finish audio busses settings for the game

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,7 +1,11 @@
 extends Control
+@onready var window_label: Label = $Settings/SmoothScrollContainer/VBoxContainer/Window/WindowModeText
+@onready var master_vol_label: Label = $Settings/SmoothScrollContainer/VBoxContainer/MasterVolume/MarginContainer2/MasterVolLabel
+@onready var music_vol_label: Label = $Settings/SmoothScrollContainer/VBoxContainer/MusicVolume/MarginContainer2/MusicVolLabel
+@onready var sfx_vol_label: Label = $Settings/SmoothScrollContainer/VBoxContainer/SFXVolume/MarginContainer2/SFXVolLabel
 
 var onWindowMode: int = 0
-@onready var window_label: Label = $Settings/SmoothScrollContainer/VBoxContainer/Window/WindowModeText
+
 
 func _on_prev_window_mode_pressed() -> void:
 	onWindowMode -= 1
@@ -36,3 +40,19 @@ func _on_next_window_mode_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
 		window_label.text = "Windowed (Borderless)"
+
+
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	master_vol_label.text =  str(int(round(value)))
+	var audio_bus := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_linear(audio_bus, value/50)
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	music_vol_label.text = str(int(round(value)))
+	var audio_bus := AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_linear(audio_bus, value/50)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	sfx_vol_label.text =  str(int(round(value)))
+	var audio_bus := AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_volume_linear(audio_bus, value/50)
