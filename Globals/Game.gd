cdc3d27b60a3df66f381ddb587332779c038de71
Bailey Miles <baileyleah542@gmail.com>
2025-11-13 12:13:56 +0000

    Add better formatting for the save

--- Globals/Game.gd
+++ Globals/Game.gd
@@ -1,42 +1,170 @@
 extends Node
 
 var default_data: Dictionary = {
-	"score" : Big.new(0),
-	"scoremantissa" : null,
-	"scoreexpo" : null,
-	"prestiges" : {
-		"nasaprestiges" : 0,
-		"weirdmageddonprestiges" : 0,
-		"showprestiges" : 0
+	"score": Big.new(0),
+	"scoremantissa": null,
+	"scoreexpo": null,
+	"prestiges": {
+		"nasaprestiges": 0,
+		"weirdmageddonprestiges": 0,
+		"showprestiges": 0
 	},
-	"trueprestiges" : 0,
-	"upgrades" : {},
-	"nasashop" : {
-		"neilarmstrong" : 0,
-		"apollo11" : 0,
-		"marsrover" : 0,
-		"iss" : 0,
-		"hubbletelescope" : 0,
-		"moonrock" : 0,
-		"howardwolowitz" : 0,
-	},
-	"weirdmageddon" : {
-		"blackhole" : 0,
-		"whitehole" : 0,
-		"neutronstar" : 0,
-		"quasars" : 0,
-		"wormhole" : 0,
-		"darkmatter" : 0,
-		"interstellar" : 0,
-	},
-	"show": {
-		"theexpanse" : 0,
-		"startrek" : 0,
-		"battlestargalatica" : 0,
-		"doctorwho" : 0,
-		"farscape" : 0,
-		"lostinspace" : 0,
-		"cowboybebop" : 0
+	"trueprestiges": 0,
+	"upgrades": {},
+	"shops": {
+		"nasashop": {
+			"neilarmstrong": {
+				"purchases": 0,
+				"generatepowermantissa": 7.5,
+				"generatepowerexpo": -1, #0.75
+				"pricemantissa": 5,
+				"priceexpo": 1     #50
+			},
+			"apollo11": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 0,   #2
+				"pricemantissa": 1,
+				"priceexpo": 2            #100
+			},
+			"marsrover": {
+				"purchases": 0,
+				"generatepowermantissa": 7,
+				"generatepowerexpo": 0,   #7
+				"pricemantissa": 2.5,
+				"priceexpo": 2            #250
+			},
+			"iss": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 1,   #20
+				"pricemantissa": 8,
+				"priceexpo": 2            #800
+			},
+			"hubbletelescope": {
+				"purchases": 0,
+				"generatepowermantissa": 5,
+				"generatepowerexpo": 1,   #50
+				"pricemantissa": 2,
+				"priceexpo": 3            #2000
+			},
+			"moonrock": {
+				"purchases": 0,
+				"generatepowermantissa": 1,
+				"generatepowerexpo": 2,   #100
+				"pricemantissa": 6,
+				"priceexpo": 3            #6000
+			},
+			"howardwolowitz": {
+				"purchases": 0,
+				"generatepowermantissa": 5,
+				"generatepowerexpo": 2,   #500
+				"pricemantissa": 2,
+				"priceexpo": 4            #20000
+			}
+		},
+		"weirdmageddon": {
+			"blackhole": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 3,   #2000
+				"pricemantissa": 5,
+				"priceexpo": 5            #500000
+			},
+			"whitehole": {
+				"purchases": 0,
+				"generatepowermantissa": 5,
+				"generatepowerexpo": 3,   #5000
+				"pricemantissa": 1,
+				"priceexpo": 6            #1000000
+			},
+			"neutronstar": {
+				"purchases": 0,
+				"generatepowermantissa": 1,
+				"generatepowerexpo": 4,   #10000
+				"pricemantissa": 5,
+				"priceexpo": 6            #5000000
+			},
+			"quasars": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 4,   #20000
+				"pricemantissa": 2,
+				"priceexpo": 7            #20000000
+			},
+			"wormhole": {
+				"purchases": 0,
+				"generatepowermantissa": 6,
+				"generatepowerexpo": 4,   #60000
+				"pricemantissa": 6,
+				"priceexpo": 7            #60000000
+			},
+			"darkmatter": {
+				"purchases": 0,
+				"generatepowermantissa": 1,
+				"generatepowerexpo": 5,   #100000
+				"pricemantissa": 2,
+				"priceexpo": 8            #200000000
+			},
+			"interstellar": {
+				"purchases": 0,
+				"generatepowermantissa": 5,
+				"generatepowerexpo": 5,   #500000
+				"pricemantissa": 1,
+				"priceexpo": 9            #1000000000
+			}
+		},
+		"show": {
+			"theexpanse": {
+				"purchases": 0,
+				"generatepowermantissa": 1,
+				"generatepowerexpo": 6,
+				"pricemantissa": 5,
+				"priceexpo": 9
+			},
+			"startrek": {
+				"purchases": 0,
+				"generatepowermantissa": 3,
+				"generatepowerexpo": 6,
+				"pricemantissa": 2,
+				"priceexpo": 10
+			},
+			"battlestargalatica": {
+				"purchases": 0,
+				"generatepowermantissa": 1,
+				"generatepowerexpo": 7,
+				"pricemantissa": 6,
+				"priceexpo": 10
+			},
+			"doctorwho": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 7,
+				"pricemantissa": 3,
+				"priceexpo": 11
+			},
+			"farscape": {
+				"purchases": 0,
+				"generatepowermantissa": 6,
+				"generatepowerexpo": 7,
+				"pricemantissa": 9,
+				"priceexpo": 11
+			},
+			"lostinspace": {
+				"purchases": 0,
+				"generatepowermantissa": 2,
+				"generatepowerexpo": 8,
+				"pricemantissa": 2,
+				"priceexpo": 12
+			},
+			"cowboybebop": {
+				"purchases": 0,
+				"generatepowermantissa": 7,
+				"generatepowerexpo": 8,
+				"pricemantissa": 5,
+				"priceexpo": 12
+			}
+		}
 	}
 }
 
@@ -60,12 +188,31 @@ func save_data() -> void:
 	data["scoremantissa"] = data["score"].mantissa
 	data["scoreexpo"] = data["score"].exponent
 	access.store_string(JSON.stringify(data, "    ", true, true))
-	
+
+func generate_clicks() -> void:
+	var total_power := Big.new(0)
+
+	for category_name in default_data["shops"].keys():
+		var category = default_data["shops"][category_name]
+		for item_name in category.keys():
+			var item = category[item_name]
+			var power = Big.new(item["generatepowermantissa"], item["generatepowerexpo"])
+			var generated = power.multiply(Big.new(item["purchases"]))
+			total_power.plusEquals(generated)
+			data["score"].plusEquals(total_power)
+
 func _ready() -> void:
 	data = load_data()
-	var timer = Timer.new()
-	timer.one_shot = false
-	timer.wait_time = 2.5
-	add_child(timer)
-	timer.start()
-	timer.connect("timeout", save_data)
+	var save_timer = Timer.new()
+	save_timer.one_shot = false
+	save_timer.wait_time = 2.5
+	add_child(save_timer)
+	save_timer.start()
+	save_timer.connect("timeout", save_data)
+
+	var generate_timer = Timer.new()
+	generate_timer.one_shot = false
+	generate_timer.wait_time = 1.0
+	add_child(generate_timer)
+	generate_timer.start()
+	generate_timer.connect("timeout", generate_clicks)
